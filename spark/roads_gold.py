06bd4d4c7f461a96475d8fb1370210622a2efe79
vidyl911 <viplove.gadkari@mail.polimi.it>
2026-09-24 01:29:46 +0530

    configuring spark & hadoop

--- spark/roads_gold.py
+++ spark/roads_gold.py
@@ -5,6 +5,7 @@ from pyspark.sql import functions as F
 spark = (
     SparkSession.builder
     .appName("UrbanMobility-Roads-Gold")
+    .config("spark.hadoop.fs.permissions.umask-mode", "000")
     .getOrCreate()
 )
 
